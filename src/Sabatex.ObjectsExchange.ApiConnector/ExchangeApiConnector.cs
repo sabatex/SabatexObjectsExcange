605bd63b19c9d7d426fa913c12b32e35ce5a7d4c
ITStep-Artu-Student <135251842+ITStep-Artu-Student@users.noreply.github.com>
2023-06-01 19:08:18 +0300

    Update ExchangeApiConnector.cs

--- src/Sabatex.ObjectsExchange.ApiConnector/ExchangeApiConnector.cs
+++ src/Sabatex.ObjectsExchange.ApiConnector/ExchangeApiConnector.cs
@@ -1,157 +1,234 @@
-﻿//(c) Serhiy Lakas (https://sabatex.github.io)
-namespace Sabatex.ObjectsExchange.ApiConnector
-{
+using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+using Sabatex.ObjectsExchange.Models;
 
-
-#if NET6_0_OR_GREATER
+namespace Sabatex.ObjectsExchange.ApiConnector;
 #nullable enable
-    using System;
-    using Sabatex.ObjectsExchange.Models;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Net;
-    using System.Net.Http;
-    using System.Net.Http.Headers;
-    using System.Net.Http.Json;
-    using System.Text;
-    using System.Threading.Tasks;
-
-    public class ExchangeApiConnector : IDisposable
+public class APIConnector
+{
+    private static readonly HttpClient client = new(new HttpClientHandler()
     {
-        private string? accessToken;
-        private readonly Guid clientId;
-        private string? refreshToken;
-        private readonly Func<Task<string>> passwordGeter;
-        private readonly Func<Token, Task> tokenUpdate;
-        private DateTime? expired_token;
+        ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => { return true; }
+    })
+    {
+        BaseAddress = new Uri("https://sabatex.francecentral.cloudapp.azure.com/")
+    };
+    private Token? UserToken = null;
+    public Guid? clientId { get; private set; }
+    private DateTime? TokenExpiration { get; set; }
 
-        private readonly HttpClient httpClient;
-        private readonly HttpClientHandler? httpClientHandler;
-
-
-        public void Dispose()
+    private void UpdateHeaders()
+    {
+        if (UserToken is not null)
         {
-            httpClient?.Dispose();
-            httpClientHandler?.Dispose();
-        }
-        #region constructor
-        public ExchangeApiConnector(ExchangeApiSettings settings, bool acceptFailCertificates, Func<string> passwordGetter) : base()
-        {
-            accessToken = settings.AccessToken;
-            clientId = new Guid(settings.ClientId);
-            refreshToken = settings.RefreshToken;
-            this.passwordGeter = passwordGeter;
-            httpClientHandler = new HttpClientHandler();
-            httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; };
-            httpClient = new HttpClient(httpClientHandler);
-            httpClient.BaseAddress = new Uri(settings.BaseUri);
-
-        }
-        #endregion
-
-        #region Autorization
-        private async Task UpdateAccessTokenAsync(Token token)
-        {
-            if (tokenUpdate != null)
-                await tokenUpdate.Invoke(token);
-            accessToken = token.AccessToken;
-            refreshToken = token.RefreshToken;
-            expired_token = DateTime.UtcNow + TimeSpan.FromSeconds(token.ExpiresIn);
-            httpClient.DefaultRequestHeaders.Add("apiToken", token.AccessToken);
-        }
-
-        private async Task<bool> AutorizeAsync()
-        {
-            var login = new Login
+            lock (client.DefaultRequestHeaders)
             {
-                ClientId = clientId,
-                Password = await passwordGeter.Invoke()
-            };
-            var response = await httpClient.PostAsJsonAsync("api/v0/login", login);
-            if (response == null) return false;
-            if (response.IsSuccessStatusCode)
-            {
-                var token = await response.Content.ReadFromJsonAsync<Token>();
-                if (token == null) return false;
-                await UpdateAccessTokenAsync(token);
-                return true;
+                if (client.DefaultRequestHeaders.Contains("clientId")) client.DefaultRequestHeaders.Remove("clientId");
+                if (client.DefaultRequestHeaders.Contains("apiToken")) client.DefaultRequestHeaders.Remove("apiToken");
+                client.DefaultRequestHeaders.Add("clientId", clientId.ToString());
+                client.DefaultRequestHeaders.Add("apiToken", UserToken.AccessToken);
             }
-            return false;
         }
+    }
 
-        private async Task<bool> RefreshTokenAsync()
-        {
-            if (refreshToken == null) return false;
-            var response = await httpClient.PostAsJsonAsync("api/v0/refresh_token", new
-            Login
-            {
-                ClientId = clientId,
-                Password = refreshToken
-            });
-            if (response.IsSuccessStatusCode)
-            {
-                var rt = await response.Content.ReadFromJsonAsync<Token>();
-                if (rt == null) return false;
-                await UpdateAccessTokenAsync(rt);
-                return true;
-            }
-            return false;
-        }
-
-        /// <summary>
-        /// speed check valid token and update invalid
-        /// </summary>
-        /// <returns></returns>
-        private async Task CheckAutorized()
-        {
-            if (accessToken != null)
-            {
-                if (expired_token > DateTime.UtcNow)
-                {
-                    return; // token is valid
-                }
-                else
-                {
-                    if (refreshToken != null)
-                    {
-                        if (await RefreshTokenAsync()) return; // update access token
-                    }
-                }
-            }
-            await AutorizeAsync(); // login with password
-        }
-        #endregion
-
-
-        /// <summary>
-        /// renev access_token by refresh token or login by password
-        /// </summary>
-        /// <returns></returns>
-
-        private async Task<HttpResponseMessage> PostAsync<T>(string? uriString, T value)
-        {
-            await CheckAutorized();
-            var response = await httpClient.PostAsJsonAsync(uriString, value);
-            if (response.StatusCode == System.Net.HttpStatusCode.Unauthorized)
-            {
-                await AutorizeAsync();
-                response = await httpClient.PostAsJsonAsync(uriString, value);
-            }
-            if (response.IsSuccessStatusCode) return response;
-            throw new Exception($"The error post object:{value}");
-        }
-
-        public async Task PostObjectAsync(string objectType, string objectId, string text) =>
-            await PostAsync("api/v0/objects",
-                                             new {
-                                                 objectType = objectType,
-                                                 objectId = objectId,
-                                                 text = text,
-                                                 dateStamp = DateTime.UtcNow
-                                             });
-
+    private void SetTokenExpiration()
+    {
+        if (UserToken is not null) TokenExpiration = DateTime.UtcNow.AddSeconds(UserToken.ExpiresIn);
 
     }
 
-#endif
+    public async Task<bool> Login(Guid newClientId, string password)
+    {
+        // Can do anything before token replacement.
+        client.DefaultRequestHeaders.Clear();
+        var response = await client.PostAsJsonAsync("api/v0/login", new { clientId = newClientId, password });
+        if (response is not null && response.IsSuccessStatusCode)
+        {
+            UserToken = await response.Content.ReadFromJsonAsync<Token>();
+            clientId = newClientId;
+            UpdateHeaders();
+            SetTokenExpiration();
+            OnTokenUpdate?.Invoke(this, UserToken);
+            return true;
+        }
+        else
+        {
+            UpdateHeaders(); // Return cleaned headers back.
+            // Token wasn't updated.
+            return false;
+        }
+    }
+
+    private bool IsTimeExpired() => TokenExpiration is not null && TokenExpiration <= DateTime.UtcNow;
+
+    public async Task<bool> IsTokenAlive()
+    {
+        if (UserToken is null)
+        {
+            // OnTokenExpiration?.Invoke(this);
+            return false;
+        }
+        else if (IsTimeExpired())
+        {
+            if (UserToken.RefreshToken is null)
+            {
+                UserToken = null;
+                clientId = null;
+                OnTokenExpiration?.Invoke(this);
+                return false;
+            }
+            else
+            {
+                var response = client.PostAsJsonAsync("api/v0/RefresToken", new { clientId, password = UserToken.RefreshToken });
+                response.Wait();
+                if (response is not null && response.Result.IsSuccessStatusCode)
+                {
+                    UserToken = await response.Result.Content.ReadFromJsonAsync<Token>();
+                    UpdateHeaders();
+                    SetTokenExpiration();
+                    OnTokenUpdate?.Invoke(this, UserToken);
+                    return true;
+                }
+                else
+                {
+                    UserToken = null;
+                    clientId = null;
+                    OnTokenExpiration?.Invoke(this);
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public event Action<object, Token?>? OnTokenUpdate;
+
+    public event Action<object>? OnTokenExpiration;
+
+    public static APIConnector GetAPIConnector()
+    {
+        return new APIConnector();
+    }
+
+    private APIConnector() { }
+
+    public async Task<long> ApiPostObjectExchangeAsync(PostObject data, Guid destinationId)
+    {
+        if (!(await IsTokenAlive())) throw new Exception("Unable to get object.");
+        if (client.DefaultRequestHeaders.Contains("destinationId")) client.DefaultRequestHeaders.Remove("destinationId");
+        client.DefaultRequestHeaders.Add("destinationId", destinationId.ToString());
+        var result = await client.PostAsJsonAsync("api/v0/objects", data);
+        if(result is not null && result.IsSuccessStatusCode)
+        {
+            return long.Parse(await result.Content.ReadAsStringAsync());
+        }
+        else throw new Exception("Unable to get object.");
+    }
+
+    public async Task<long> ApiPostObjectExchangeAsync(string objectType, string objectId, string text, DateTime dateStamp, Guid destinationId)
+    {
+        if (!(await IsTokenAlive())) throw new Exception("Unable to get object.");
+        if (client.DefaultRequestHeaders.Contains("destinationId")) client.DefaultRequestHeaders.Remove("destinationId");
+        client.DefaultRequestHeaders.Add("destinationId", destinationId.ToString());
+        var result = await client.PostAsJsonAsync("api/v0/objects", new { objectType, objectId, text, dateStamp });
+        if(result is not null && result.IsSuccessStatusCode)
+        {
+            return long.Parse(await result.Content.ReadAsStringAsync());
+        }
+        else throw new Exception("Unable to get object.");
+    }
+
+    public async Task<ObjectExchange[]?> ApiGetObjectExchangeAsync()
+    {
+        if (!(await IsTokenAlive())) return null;
+        return await client.GetFromJsonAsync<ObjectExchange[]>("api/v0/objects");
+    }
+
+    public async Task<ObjectExchange[]?> ApiGetObjectExchangeAsync(int take = 10)
+    {
+        if (!(await IsTokenAlive())) return null;
+        return await client.GetFromJsonAsync<ObjectExchange[]>($"api/v0/objects?take={take}");
+    }
+
+    public async Task<ObjectExchange[]?> ApiGetObjectExchangeAsync(string nodeName, int take = 10)
+    {
+        if (!(await IsTokenAlive())) return null;
+        return await client.GetFromJsonAsync<ObjectExchange[]>($"api/v0/objects?take={take}&nodeName={nodeName}");
+    }
+
+    public async Task<bool> ApiDeleteObjectExchangeAsync(long deletedId)
+    {
+        if (!(await IsTokenAlive())) return false;
+        var result = await client.DeleteAsync($"api/v0/objects/{deletedId}");
+        return result is not null && result.IsSuccessStatusCode;
+    }
+
+    public async Task<bool> ApiDeleteObjectsExchangeAsync()
+    {
+        if (!(await IsTokenAlive())) return false;
+        var got = await client.GetFromJsonAsync<ObjectExchange[]>("api/v0/objects") ?? Array.Empty<ObjectExchange>();
+        foreach (var current in got)
+        {
+            bool temporary = await ApiDeleteObjectExchangeAsync(current.Id);
+            if (temporary == false) return false;
+        }
+        return true;
+    }
+
+    public async Task<long> ApiPostQueryAsync(PostObject data, Guid destinationId)
+    {
+        if (!(await IsTokenAlive())) throw new Exception("Unable to get object.");
+        var result = await client.PostAsJsonAsync("api/v0/queries", data);
+        if(result is not null && result.IsSuccessStatusCode)
+        {
+            return long.Parse(await result.Content.ReadAsStringAsync());
+        }
+        else throw new Exception("Unable to get object.");
+    }
+
+    public async Task<long> ApiPostQueryAsync(string objectType, string objectId, Guid destinationId)
+    {
+        if (!(await IsTokenAlive())) throw new Exception("Unable to get object.");
+        var result = await client.PostAsJsonAsync("api/v0/queries", new { objectType, objectId });
+        if(result is not null && result.IsSuccessStatusCode)
+        {
+            return long.Parse(await result.Content.ReadAsStringAsync());
+        }
+        else throw new Exception("Unable to get object.");
+    }
+
+    public async Task<QueryObject[]?> ApiGetQueryObjectAsync()
+    {
+        if (!(await IsTokenAlive())) return null;
+        // Ask if line lower has any use in this context.
+        if (client.DefaultRequestHeaders.Contains("destinationId")) client.DefaultRequestHeaders.Remove("destinationId");
+        return await client.GetFromJsonAsync<QueryObject[]>($"api/v0/queries");
+    }
+
+    public async Task<QueryObject[]?> ApiGetQueryObjectAsync(int take = 10)
+    {
+        if (!(await IsTokenAlive())) return null;
+        // Ask if line lower has any use in this context.
+        if (client.DefaultRequestHeaders.Contains("destinationId")) client.DefaultRequestHeaders.Remove("destinationId");
+        return await client.GetFromJsonAsync<QueryObject[]>($"api/v0/queries?take={take}");
+    }
+
+    public async Task<QueryObject[]?> ApiGetQueryObjectAsync(string nodeName, int take = 10)
+    {
+        if (!(await IsTokenAlive())) return null;
+        // Ask if line lower has any use in this context.
+        if (client.DefaultRequestHeaders.Contains("destinationId")) client.DefaultRequestHeaders.Remove("destinationId");
+        return await client.GetFromJsonAsync<QueryObject[]>($"api/v0/queries?take={take}&nodeName={nodeName}");
+    }
+
+    public async Task<bool> ApiDeleteQueryObjectAsync(long deletedId)
+    {
+        if (!(await IsTokenAlive())) return false;
+        // Ask if line lower has any use in this context.
+        if (client.DefaultRequestHeaders.Contains("destinationId")) client.DefaultRequestHeaders.Remove("destinationId");
+        var result = await client.DeleteAsync($"api/v0/queries/{deletedId}");
+        return result is not null && result.IsSuccessStatusCode;
+    }
 }
