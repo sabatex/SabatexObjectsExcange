c87f072a5025ae56062680fca614532be943d350
Serhiy Lakas <serhiy.lakas@hotmail.com>
2026-06-01 20:55:00 +0300

    Add XML docs, update APIs, and bump version to 10.2.0
    Extensive XML documentation added to core interfaces and classes for improved clarity. Updated `IObjectAnalizer.AnalyzeAsync` to accept an optional result parameter. Commented out `GetObjectAnalizer` in `NodeDescriptor`. Bumped project and dependency versions to 10.2.0.

--- Sabatex.ObjectExchange.Core/IObjectAnalizer.cs
+++ Sabatex.ObjectExchange.Core/IObjectAnalizer.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +6,16 @@ using System.Threading.Tasks;
 
 namespace Sabatex.ObjectExchange.Core
 {
+    /// <summary>
+    /// Defines an analyzer for objects.
+    /// </summary>
     public interface IObjectAnalizer
     {
-        public Task<AnalizeResult> AnalyzeAsync();
+        /// <summary>
+        /// Analyzes an object asynchronously.
+        /// </summary>
+        /// <param name="result" >Optional initial analysis result to be used as a starting point for the analysis. If null, a new analysis will be performed.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains the analysis result.</returns>
+        public Task<AnalizeResult> AnalyzeAsync(AnalizeResult? result);
     }
 }
--- Sabatex.ObjectExchange.Core/NodeDescriptor.cs
+++ Sabatex.ObjectExchange.Core/NodeDescriptor.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,16 +6,28 @@ using System.Threading.Tasks;
 
 namespace Sabatex.ObjectExchange.Core
 {
+    /// <summary>
+    /// Represents a descriptor for a node in the object exchange system. This class is responsible for managing analyzers associated with different object types and providing access to these analyzers based on the object type.
+    /// </summary>
     public class NodeDescriptor
     {
         private readonly Dictionary<string,Type> _analizers = new Dictionary<string, Type>();
         private readonly Guid _destinationId;
+        /// <summary>
+        /// Initializes a new instance of the NodeDescriptor class with the specified destination ID. The destination ID is used to identify the node in the object exchange system.
+        /// </summary>
+        /// <param name="destinationId">The unique identifier for the destination node.</param>
         public NodeDescriptor(Guid destinationId)
         {
             _destinationId = destinationId;
         }
 
-
+        /// <summary>
+        /// Registers an analyzer for a specific object type. The analyzer must implement the IObjectAnalizer interface. If an analyzer for the specified object type is already registered, an ArgumentException is thrown.
+        /// </summary>
+        /// <typeparam name="T">The type of the analyzer to register.</typeparam>
+        /// <param name="objectType">The object type for which the analyzer is being registered.</param>
+        /// <exception cref="ArgumentException">Thrown if an analyzer for the specified object type is already registered.</exception>
         public void RegisterAnalizer<T>(string objectType) where T : IObjectAnalizer
         {
             if (_analizers.ContainsKey(objectType.ToLower()))
@@ -24,7 +36,12 @@ namespace Sabatex.ObjectExchange.Core
             }
             _analizers.Add(objectType.ToLower(), typeof(T));
         }
-
+        /// <summary>
+        /// Retrieves the analyzer type associated with the specified object type. If no analyzer is found for the given object type, an exception is thrown.
+        /// </summary>
+        /// <param name="objectType">The object type for which to retrieve the analyzer type.</param>
+        /// <returns>The type of the analyzer associated with the specified object type.</returns>
+        /// <exception cref="Exception">Thrown if no analyzer is found for the specified object type.</exception>
         public Type GetAnalizerType(string objectType)
         {
             if (_analizers.TryGetValue(objectType.ToLower(), out var analizer))
@@ -33,14 +50,14 @@ namespace Sabatex.ObjectExchange.Core
             }
             throw new Exception($"Не знайдено аналізатор для типу {objectType}");
         }
-
-        public IObjectAnalizer GetObjectAnalizer(string objectType)
-        {
-            if (_analizers.TryGetValue(objectType.ToLower(), out var analizer))
-            {
-                return (Activator.CreateInstance(analizer) ?? throw new Exception("Uknown error if create analizer")) as IObjectAnalizer ?? throw new Exception("The analizator not contains interface IObjectAnalizer");
-            }
-            throw new Exception($"Не знайдено аналізатор для типу {objectType}");
-        }
+        
+        //public IObjectAnalizer GetObjectAnalizer(string objectType)
+        //{
+        //    if (_analizers.TryGetValue(objectType.ToLower(), out var analizer))
+        //    {
+        //        return (Activator.CreateInstance(analizer) ?? throw new Exception("Uknown error if create analizer")) as IObjectAnalizer ?? throw new Exception("The analizator not contains interface IObjectAnalizer");
+        //    }
+        //    throw new Exception($"Не знайдено аналізатор для типу {objectType}");
+        //}
     }
 }
